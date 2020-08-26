76ff990577dc92350aeb8748c67a9d51a8884388
liyuan <liyuan5768@163.com>
2020-08-26 17:31:16 +0800

    liyuan appitem

---
+++ js/order.js
@@ -0,0 +1,14 @@
+function getID(el) {
+    return document.getElementById(el);
+}
+function getTag(el) {
+    return document.getElementsByTagName(el);
+}
+function getClass(el) {
+    return document.getElementsByClassName(el);
+}
+function tab() {
+    let oTab = getID('tab');
+    let 
+}
+tab();
